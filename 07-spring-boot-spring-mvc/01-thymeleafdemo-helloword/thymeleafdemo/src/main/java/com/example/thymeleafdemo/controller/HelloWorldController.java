e9a6bed7515c9f9f3cbcac52a0e0c837e9be836d
Kevin Job DUBUCHE <kevin-job.dubuche@fortil.group>
2024-01-10 10:43:45 +0100

    Spring Boot - Request Params

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/thymeleafdemo/src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/thymeleafdemo/src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.example.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -13,6 +16,26 @@ public class HelloWorldController {
 
     @RequestMapping("processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+        String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+        String result = "Yo! " + theName;
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName,
+                                          Model model){
+        theName = theName.toUpperCase();
+        String result = "Hey my friend from v3! " + theName;
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 }
