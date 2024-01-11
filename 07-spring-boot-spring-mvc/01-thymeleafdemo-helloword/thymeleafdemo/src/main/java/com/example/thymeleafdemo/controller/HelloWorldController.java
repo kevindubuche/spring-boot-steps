2145f8c8e0871e3ee5d6f2e939af8d9d5083a9b7
Kevin Job DUBUCHE <kevin-job.dubuche@fortil.group>
2024-01-11 09:17:10 +0100

    Spring Data binding

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/thymeleafdemo/src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/thymeleafdemo/src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
@@ -3,13 +3,15 @@ package com.example.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping("showForm")
+    @GetMapping("showForm")
     public String showForm() {
         return "helloworld-form";
     }
@@ -29,7 +31,7 @@ public class HelloWorldController {
         return "helloworld";
     }
 
-    @RequestMapping("/processFormVersionThree")
+    @PostMapping("/processFormVersionThree")
     public String processFormVersionThree(@RequestParam("studentName") String theName,
                                           Model model){
         theName = theName.toUpperCase();
